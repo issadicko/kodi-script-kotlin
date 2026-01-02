fdfe97b88c377d43de2e43ef92b428bf239105cc
dickode78 <dickode78@gmail.com>
2026-01-02 16:17:17 +0000

    chore: bump version to 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 group = "io.github.issadicko"
-version = "0.1.0"
+version = "0.1.1"
 
 // Load Maven Central credentials from local file
 val mavenCentralCredentials = Properties().apply {
